0aea6952043df91010bc742b0d8ce8dd47e67b83
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-01-06 19:06:41 -0200

    :rocket: Adiciona comando para limpar console.

---
+++ Ferret/Ferret.View/CommandParser/Option/ClearOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+using Ferret.View.CommandParser.Base;
+
+namespace Ferret.View.CommandParser.Option
+{
+    internal sealed class ClearOptions : IOptions
+    {
+        [Option("clean", Required = true)]
+        public bool ClearConsole { get; set; }
+    }
+}
---
+++ Ferret/Ferret.View/Core/Services/ScreenCleaner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ferret.View.CommandParser.Base;
+using Ferret.View.Core.Base;
+using Ferret.View.CommandParser.Option;
+
+namespace Ferret.View.Core.Services
+{
+    internal sealed class ScreenCleaner : IPinpadService
+    {
+        public IOptions Options { get; private set; } = new ClearOptions();
+
+        public void Execute()
+        {
+            Console.Clear();
+        }
+    }
+}
