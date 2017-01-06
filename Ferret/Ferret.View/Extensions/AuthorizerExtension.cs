9f00cc3269b8c9f2df9ad7cb420502e516980ca3
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-01-06 19:36:29 -0200

    :memo: Extension methods documentation.

--- Ferret/Ferret.View/Extensions/AuthorizerExtension.cs
+++ Ferret/Ferret.View/Extensions/AuthorizerExtension.cs
@@ -7,8 +7,15 @@ using Ferret.View.KsnStuff;
 
 namespace Ferret.View.Extensions
 {
+    /// <summary>
+    /// Stuff easely do with <see cref="ICardPaymentAuthorizer"/>.
+    /// </summary>
     internal static class AuthorizerExtension
     {
+        /// <summary>
+        /// Show a brief description to each pinpad.
+        /// </summary>
+        /// <param name="pinpads">Pinpads to log on console.</param>
         public static void ShowSummaryOnConsole(this ICollection<ICardPaymentAuthorizer> pinpads)
         {
             Console.WriteLine(
@@ -18,19 +25,24 @@ namespace Ferret.View.Extensions
                        Manufacturer = s.PinpadFacade.Infos.ManufacturerName.Replace(" ", ""),
                        SerialNumber = s.PinpadFacade.Infos.SerialNumber.Replace(" ", "")
                    })
-                   .ToMarkdownTable());
+                .ToMarkdownTable());
         }
+        /// <summary>
+        /// Log acquirers and KSN information on console.
+        /// </summary>
+        /// <param name="pinpads">Pinpads to log on console.</param>
+        /// <param name="acquirers">KSN information obtained from ABECS GDU command.</param>
         public static void ShowKsns(this ICollection<ICardPaymentAuthorizer> pinpads,
             List<Acquirer> acquirers)
         {
             // TODO: Mostrar dados do KSN.
             Console.WriteLine(
-                   pinpads.Select(s => new
-                   {
-                       PortName = s.PinpadFacade.Communication.PortName,
-                       SerialNumber = s.PinpadFacade.Infos.SerialNumber.Replace(" ", "")
-                   })
-                   .ToMarkdownTable());
+                    pinpads.Select(s => new
+                    {
+                        PortName = s.PinpadFacade.Communication.PortName,
+                        SerialNumber = s.PinpadFacade.Infos.SerialNumber.Replace(" ", "")
+                    })
+                .ToMarkdownTable());
         }
     }
 }
