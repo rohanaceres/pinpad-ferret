256927431afa3901ac1106da89276444c8ce5c12
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-01-06 19:57:11 -0200

    :construction: Adiciona serviço para verificar se uma adquirente específica é suportada pelo pinpad.

---
+++ Ferret/Ferret.View/CommandParser/Option/AcquirerVerifierOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using Ferret.View.CommandParser.Base;
+
+namespace Ferret.View.CommandParser.Option
+{
+    /// <summary>
+    /// Verify if a specific aquirer exist on pinpad memory.
+    /// </summary>
+    internal sealed class AcquirerVerifierOptions : IOptions
+    {
+        /// <summary>
+        /// Name of the acquirer to search.
+        /// </summary>
+        [Option("hasAcquirer", Required = true)]
+        public string HasAcquirer { get; set; }
+    }
+}
---
+++ Ferret/Ferret.View/Core/Services/AcquirerVerifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Ferret.View.CommandParser.Base;
+using Ferret.View.Core.Base;
+using Ferret.View.CommandParser.Option;
+
+namespace Ferret.View.Core.Services
+{
+    internal sealed class AcquirerVerifier : IPinpadService
+    {
+        public IOptions Options { get; private set; } = new AcquirerVerifierOptions();
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
