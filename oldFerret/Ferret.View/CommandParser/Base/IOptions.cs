ac511babae5089df3f3332fcc265917e8993da3d
Ceres Rohana <ceres.rohana@gmail.com>
2017-09-05 17:22:11 -0300

    :fire: Retira projeto antigo.

--- oldFerret/Ferret.View/CommandParser/Base/IOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ferret.View.CommandParser.Base
-{
-    /// <summary>
-    /// Used only to define a set of options, that for now has nothing in common.
-    /// </summary>
-    internal interface IOptions { }
-}
